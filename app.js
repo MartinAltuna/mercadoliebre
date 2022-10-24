ba9e31861fb0461928f8ef2a030e78cec9eb0767
MartinAltuna <martin.altuna98@gmail.com>
2022-10-24 17:19:02 -0300

    Agregue html de login y register

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const path = require('path');
+const PUBLICFOLDER = path.resolve('public')
+app.use(express.static(PUBLICFOLDER))
 
 
 app.listen(3000, ()=>{
@@ -10,3 +13,11 @@ app.listen(3000, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
+
+app.get('/register', (req, res)=>{
+    res.sendFile(path.resolve('views/register.html'))
+})
+
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve('views/login.html'))
+})
